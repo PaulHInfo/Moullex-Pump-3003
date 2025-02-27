bd9903c455cbd7c4ca92606cda9713a797725f6d
PaulHinfo <paulHuwiler.info@gmail.com>
2025-02-27 21:31:10 +0100

    création du JSON

---
+++ data.js
@@ -0,0 +1,58 @@
+{
+    "bras": {
+        "exo": [
+            {
+                "name": "Pompes diamant",
+                "obj": false,
+                "objListe": [],
+                "imgB64": ""
+            },
+            {
+                "name": "Dips entre deux chaises",
+                "obj": true,
+                "objListe": [
+                    {
+                        "objNom": "2 chaises"
+                    }
+                ],
+                "imgB64": ""
+            },            
+            {
+                "name": "Curl biceps avec des bouteilles d’eau",
+                "obj": true,
+                "objListe": [
+                    {
+                        "objNom": "2 bouteilles d'eau"
+                    }
+                ],
+                "imgB64": ""
+            }
+        ]
+    },
+    "pec": {
+        "exo": [
+            {
+                "name": "Pompes inclinées (mains surélevées, accent sur le bas des pecs)",
+                "obj": false,
+                "objListe": [],
+                "imgB64": ""
+            },
+            {
+                "name": "Pompes déclinées (pieds surélevés, accent sur le haut des pecs)",
+                "obj": false,
+                "objListe": [],
+                "imgB64": ""
+            },            
+            {
+                "name": "Développé couché avec bouteilles d'eau",
+                "obj": true,
+                "objListe": [
+                    {
+                        "objNom": "2 bouteilles d'eau"
+                    }
+                ],
+                "imgB64": ""
+            }
+        ]
+    }
+}
