8c690681212c9e29879275257018cbaeae3668cc
Paul <paulhuwiler.info@gmail.com>
2025-03-03 23:05:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from worker import sport
 
+TOKEN = ""
+
 # Configurez les intents
 intents = discord.Intents.default()
 intents.message_content = True  # Nécessaire pour lire le contenu des messages
@@ -21,15 +23,15 @@ async def on_message(message):
     await bot.process_commands(message)  # Nécessaire pour traiter les commandes
 
 # Commande !sport avec récupération du texte du message
-@bot.command(name='sport')
-async def sport(ctx, *, message: str = None):
+@bot.command(name='train')
+async def train(ctx, *, message: str = None):
     #print(f"Commande !sport détectée avec le message : {message}")  # Log pour débogage
     if message:
         # Si un message est fourni après !sport
         #await ctx.send(f"Vous avez choisi le sport : {message} !")
         #sport.chekCommande("message")
         await ctx.send("ok")
-        t = sport.chekCommande("message")
+        t = await  sport.chekCommande("message")
         print(t)
     else:
         # Si aucun message n'est fourni après !sport
